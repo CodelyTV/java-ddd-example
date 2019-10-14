d05cadd3db17c3a00a7f53e6bb9025d01e2bca3d
Rafa Gómez <rgomezcasas@gmail.com>
2019-10-14 12:49:55 +0200

    Simplify mysql step repository test using "data providers"

--- src/mooc/test/tv/codely/mooc/steps/infrastructure/persistence/MySqlStepRepositoryShould.java
+++ src/mooc/test/tv/codely/mooc/steps/infrastructure/persistence/MySqlStepRepositoryShould.java
@@ -5,12 +5,16 @@ import tv.codely.mooc.courses.domain.Course;
 import tv.codely.mooc.courses.domain.CourseIdMother;
 import tv.codely.mooc.courses.domain.CourseMother;
 import tv.codely.mooc.steps.StepsModuleInfrastructureTestCase;
+import tv.codely.mooc.steps.domain.Step;
 import tv.codely.mooc.steps.domain.StepIdMother;
 import tv.codely.mooc.steps.domain.challenge.ChallengeStep;
 import tv.codely.mooc.steps.domain.challenge.ChallengeStepMother;
 import tv.codely.mooc.steps.domain.video.VideoStep;
 import tv.codely.mooc.steps.domain.video.VideoStepMother;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -18,39 +22,27 @@ import static org.junit.jupiter.api.Assertions.assertFalse;
 
 final class MySqlStepRepositoryShould extends StepsModuleInfrastructureTestCase {
     @Test
-    void save_a_video_step() {
-        VideoStep step = VideoStepMother.random();
-
-        repository.save(step);
+    void save_a_step() {
+        for (Step step : steps()) {
+            repository.save(step);
+        }
     }
 
     @Test
-    void save_a_challenge_step() {
-        ChallengeStep step = ChallengeStepMother.random();
+    void return_an_existing_step() {
+        for (Step step : steps()) {
+            repository.save(step);
 
-        repository.save(step);
-    }
-
-    @Test
-    void return_an_existing_challenge_step() {
-        ChallengeStep step = ChallengeStepMother.random();
-
-        repository.save(step);
-
-        assertEquals(Optional.of(step), repository.search(step.id()));
-    }
-
-    @Test
-    void return_an_existing_video_step() {
-        VideoStep step = VideoStepMother.random();
-
-        repository.save(step);
-
-        assertEquals(Optional.of(step), repository.search(step.id()));
+            assertEquals(Optional.of(step), repository.search(step.id()));
+        }
     }
 
     @Test
     void not_return_a_non_existing_course() {
         assertFalse(repository.search(StepIdMother.random()).isPresent());
+    }
+
+    private List<? extends Step> steps() {
+        return Arrays.asList(ChallengeStepMother.random(), VideoStepMother.random());
     }
 }
