f58b63263829de23500a7a6ddd4b311fea9bc354
Rafa Gómez <rgomezcasas@gmail.com>
2019-10-28 00:03:46 +0100

    PoC - Integrate RabbitMQ

---
+++ src/shared/main/tv/codely/shared/infrastructure/bus/event/DomainEventJsonSerializer.java
@@ -0,0 +1,9 @@
+package tv.codely.shared.infrastructure.bus.event;
+
+import tv.codely.shared.domain.bus.event.DomainEvent;
+
+public final class DomainEventJsonSerializer {
+    public static String serialize(DomainEvent domainEvent) {
+        return "";
+    }
+}
