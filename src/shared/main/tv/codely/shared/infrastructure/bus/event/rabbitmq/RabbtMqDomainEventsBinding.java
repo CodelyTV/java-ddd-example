8e56961104ee12808530d87bf02d703c9d297586
Rafa Gómez <rgomezcasas@gmail.com>
2019-10-29 10:04:50 +0100

    Publish domain events to RabbitMQ

--- src/shared/main/tv/codely/shared/infrastructure/bus/event/rabbitmq/RabbtMqDomainEventsBinding.java
+++
@@ -1,9 +0,0 @@
-package tv.codely.shared.infrastructure.bus.event.rabbitmq;
-
-import org.springframework.cloud.stream.annotation.Output;
-import org.springframework.messaging.MessageChannel;
-
-interface RabbtMqDomainEventsBinding {
-    @Output("domain_events")
-    MessageChannel domainEvents();
-}
