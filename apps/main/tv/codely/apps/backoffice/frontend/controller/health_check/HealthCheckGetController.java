13087b34fec2c8da15a0097eeb00fb7d25fcbc52
Rafa Gómez <rgomezcasas@gmail.com>
2019-11-11 09:14:33 +0100

    Add backoffice frontend application

---
+++ apps/main/tv/codely/apps/backoffice/frontend/controller/health_check/HealthCheckGetController.java
@@ -0,0 +1,18 @@
+package tv.codely.apps.backoffice.frontend.controller.health_check;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+
+@RestController
+public final class HealthCheckGetController {
+
+    @GetMapping("/health-check")
+    public HashMap<String, String> index() {
+        HashMap<String, String> status = new HashMap<>();
+        status.put("status", "ok");
+
+        return status;
+    }
+}
