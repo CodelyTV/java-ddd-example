7455ae30c9ab472dc6206fcd1bd74396676174cb
Rafa Gómez <rgomezcasas@gmail.com>
2019-11-11 12:37:26 +0100

    [backoffice-frontend] Add courses counter element with real data

--- apps/main/tv/codely/apps/backoffice/frontend/controller/courses/CoursesGetWebController.java
+++ apps/main/tv/codely/apps/backoffice/frontend/controller/courses/CoursesGetWebController.java
@@ -3,18 +3,30 @@ package tv.codely.apps.backoffice.frontend.controller.courses;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
+import tv.codely.mooc.courses_counter.application.find.CoursesCounterResponse;
+import tv.codely.mooc.courses_counter.application.find.FindCoursesCounterQuery;
+import tv.codely.shared.domain.bus.query.QueryBus;
+import tv.codely.shared.domain.bus.query.QueryNotRegisteredError;
 
 import java.io.Serializable;
 import java.util.HashMap;
 
 @Controller
 public final class CoursesGetWebController {
+    private final QueryBus bus;
+
+    public CoursesGetWebController(QueryBus bus) {
+        this.bus = bus;
+    }
+
     @GetMapping("/courses")
-    public ModelAndView index() {
+    public ModelAndView index() throws QueryNotRegisteredError {
+        CoursesCounterResponse counterResponse = bus.ask(new FindCoursesCounterQuery());
+
         return new ModelAndView("pages/courses", new HashMap<String, Serializable>() {{
             put("title", "Courses");
             put("description", "Courses CodelyTV - Backoffice");
-            put("courses_counter", 5);
+            put("courses_counter", counterResponse.total());
         }});
     }
 }
