32fa519a5d94b8c4e0bbbf368408beae0a7645cb
Rafa Gómez <rgomezcasas@gmail.com>
2019-11-11 12:31:13 +0100

    [backoffice-frontend] Add courses page

---
+++ apps/main/tv/codely/apps/backoffice/frontend/controller/courses/CoursesGetWebController.java
@@ -0,0 +1,19 @@
+package tv.codely.apps.backoffice.frontend.controller.courses;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.io.Serializable;
+import java.util.HashMap;
+
+@Controller
+public final class CoursesGetWebController {
+    @GetMapping("/courses")
+    public ModelAndView index() {
+        return new ModelAndView("pages/courses", new HashMap<String, Serializable>() {{
+            put("title", "Courses");
+            put("description", "Courses CodelyTV - Backoffice");
+        }});
+    }
+}
