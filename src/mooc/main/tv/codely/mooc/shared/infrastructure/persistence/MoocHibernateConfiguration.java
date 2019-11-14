91829dabc75b3d9019bad225472c6d7613e61763
Rafa Gómez <rgomezcasas@gmail.com>
2019-11-14 22:15:47 +0100

    Abstract hibernate configuration factory

--- src/mooc/main/tv/codely/mooc/shared/infrastructure/persistence/MoocHibernateConfiguration.java
+++ src/mooc/main/tv/codely/mooc/shared/infrastructure/persistence/MoocHibernateConfiguration.java
@@ -1,130 +1,31 @@
 package tv.codely.mooc.shared.infrastructure.persistence;
 
-import io.github.cdimascio.dotenv.Dotenv;
-import org.apache.tomcat.dbcp.dbcp2.BasicDataSource;
-import org.hibernate.cfg.AvailableSettings;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.io.FileSystemResource;
-import org.springframework.core.io.Resource;
-import org.springframework.core.io.support.ResourcePatternResolver;
-import org.springframework.orm.hibernate5.HibernateTransactionManager;
 import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
-import tv.codely.shared.infrastructure.config.Parameter;
-import tv.codely.shared.infrastructure.config.ParameterNotExist;
+import tv.codely.shared.infrastructure.hibernate.HibernateConfigurationFactory;
 
 import javax.sql.DataSource;
-import java.io.File;
-import java.io.IOException;
-import java.net.URISyntaxException;
-import java.util.*;
-import java.util.stream.Collectors;
 
 @Configuration
 @EnableTransactionManagement
 public class MoocHibernateConfiguration {
-    private final Parameter config;
-    private final ResourcePatternResolver resourceResolver;
+    private final HibernateConfigurationFactory factory = new HibernateConfigurationFactory();
 
-    public MoocHibernateConfiguration(Parameter config, ResourcePatternResolver resourceResolver) {
-        this.config = config;
-        this.resourceResolver = resourceResolver;
+    @Bean
+    public PlatformTransactionManager hibernateTransactionManager() {
+        return factory.hibernateTransactionManager(sessionFactory());
     }
 
     @Bean
-    public LocalSessionFactoryBean sessionFactory() throws ParameterNotExist, IOException, URISyntaxException {
-        LocalSessionFactoryBean sessionFactory = new LocalSessionFactoryBean();
-        sessionFactory.setDataSource(dataSource());
-        sessionFactory.setHibernateProperties(hibernateProperties());
-        List<Resource> mappingFiles = searchMappingFiles("mooc");
-
-        sessionFactory.setMappingLocations(mappingFiles.toArray(new Resource[mappingFiles.size()]));
-
-        return sessionFactory;
-    }
-
-    private List<Resource> searchMappingFiles(String contextName) {
-        List<String> modules   = subdirectoriesFor(contextName);
-        List<String> goodPaths = new ArrayList<>();
-
-        for (String module : modules) {
-            String[] files = mappingFilesIn(module + "/infrastructure/persistence/hibernate/");
-
-            for (String file : files) {
-                goodPaths.add(module + "/infrastructure/persistence/hibernate/" + file);
-            }
-        }
-
-        return goodPaths.stream().map(FileSystemResource::new).collect(Collectors.toList());
-    }
-
-    private List<String> subdirectoriesFor(String contextName) {
-        String path = "./src/" + contextName + "/main/tv/codely/" + contextName + "/";
-
-        String[] files = new File(path).list((current, name) -> new File(current, name).isDirectory());
-
-        if (null == files) {
-            path  = "./main/tv/codely/" + contextName + "/";
-            files = new File(path).list((current, name) -> new File(current, name).isDirectory());
-        }
-
-        if (null == files) {
-            return Collections.emptyList();
-        }
-
-        String finalPath = path;
-
-        return Arrays.stream(files).map(file -> finalPath + file).collect(Collectors.toList());
-    }
-
-    private String[] mappingFilesIn(String path) {
-        String[] files = new File(path).list((current, name) -> new File(current, name).getName().contains(".hbm.xml"));
-
-        if (null == files) {
-            return new String[0];
-        }
-
-        return files;
+    public LocalSessionFactoryBean sessionFactory() {
+        return factory.sessionFactory("mooc", dataSource());
     }
 
     @Bean
-    public DataSource dataSource() throws ParameterNotExist, IOException {
-        BasicDataSource dataSource = new BasicDataSource();
-        dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
-        dataSource.setUrl(
-            String.format(
-                "jdbc:mysql://%s:%s/%s?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC",
-                config.get("MOOC_DATABASE_HOST"),
-                config.get("MOOC_DATABASE_PORT"),
-                config.get("MOOC_DATABASE_NAME")
-            )
-        );
-        dataSource.setUsername(config.get("MOOC_DATABASE_USER"));
-        dataSource.setPassword(config.get("MOOC_DATABASE_PASSWORD"));
-
-        Resource mysqlResource = resourceResolver.getResource("classpath:database/mooc.sql");
-        String mysqlSentences  = new Scanner(mysqlResource.getInputStream(), "UTF-8").useDelimiter("\\A").next();
-
-        dataSource.setConnectionInitSqls(new ArrayList<>(Arrays.asList(mysqlSentences.split(";"))));
-
-        return dataSource;
-    }
-
-    @Bean
-    public PlatformTransactionManager hibernateTransactionManager() throws ParameterNotExist, IOException, URISyntaxException {
-        HibernateTransactionManager transactionManager = new HibernateTransactionManager();
-        transactionManager.setSessionFactory(sessionFactory().getObject());
-        return transactionManager;
-    }
-
-    private Properties hibernateProperties() {
-        Properties hibernateProperties = new Properties();
-        hibernateProperties.put(AvailableSettings.HBM2DDL_AUTO, "none");
-        hibernateProperties.put(AvailableSettings.SHOW_SQL, "false");
-        hibernateProperties.put(AvailableSettings.DIALECT, "org.hibernate.dialect.MySQL8Dialect");
-
-        return hibernateProperties;
+    public DataSource dataSource() {
+        return factory.dataSource("localhost", 3306, "mooc", "root", "");
     }
 }
---
+++ src/shared/main/tv/codely/shared/infrastructure/hibernate/HibernateConfigurationFactory.java
@@ -0,0 +1,105 @@
+package tv.codely.shared.infrastructure.hibernate;
+
+import org.apache.tomcat.dbcp.dbcp2.BasicDataSource;
+import org.hibernate.cfg.AvailableSettings;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.core.io.Resource;
+import org.springframework.orm.hibernate5.HibernateTransactionManager;
+import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
+import org.springframework.transaction.PlatformTransactionManager;
+
+import javax.sql.DataSource;
+import java.io.File;
+import java.util.*;
+import java.util.stream.Collectors;
+
+public final class HibernateConfigurationFactory {
+    public PlatformTransactionManager hibernateTransactionManager(LocalSessionFactoryBean sessionFactory) {
+        HibernateTransactionManager transactionManager = new HibernateTransactionManager();
+        transactionManager.setSessionFactory(sessionFactory.getObject());
+
+        return transactionManager;
+    }
+
+    public LocalSessionFactoryBean sessionFactory(String contextName, DataSource dataSource) {
+        LocalSessionFactoryBean sessionFactory = new LocalSessionFactoryBean();
+        sessionFactory.setDataSource(dataSource);
+        sessionFactory.setHibernateProperties(hibernateProperties());
+
+        List<Resource> mappingFiles = searchMappingFiles(contextName);
+
+        sessionFactory.setMappingLocations(mappingFiles.toArray(new Resource[mappingFiles.size()]));
+
+        return sessionFactory;
+    }
+
+    public DataSource dataSource(String host, Integer port, String databaseName, String username, String password) {
+        BasicDataSource dataSource = new BasicDataSource();
+        dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
+        dataSource.setUrl(
+            String.format(
+                "jdbc:mysql://%s:%s/%s?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC",
+                host,
+                port,
+                databaseName
+            )
+        );
+        dataSource.setUsername(username);
+        dataSource.setPassword(password);
+
+        return dataSource;
+    }
+
+    private List<Resource> searchMappingFiles(String contextName) {
+        List<String> modules   = subdirectoriesFor(contextName);
+        List<String> goodPaths = new ArrayList<>();
+
+        for (String module : modules) {
+            String[] files = mappingFilesIn(module + "/infrastructure/persistence/hibernate/");
+
+            for (String file : files) {
+                goodPaths.add(module + "/infrastructure/persistence/hibernate/" + file);
+            }
+        }
+
+        return goodPaths.stream().map(FileSystemResource::new).collect(Collectors.toList());
+    }
+
+    private List<String> subdirectoriesFor(String contextName) {
+        String path = "./src/" + contextName + "/main/tv/codely/" + contextName + "/";
+
+        String[] files = new File(path).list((current, name) -> new File(current, name).isDirectory());
+
+        if (null == files) {
+            path  = "./main/tv/codely/" + contextName + "/";
+            files = new File(path).list((current, name) -> new File(current, name).isDirectory());
+        }
+
+        if (null == files) {
+            return Collections.emptyList();
+        }
+
+        String finalPath = path;
+
+        return Arrays.stream(files).map(file -> finalPath + file).collect(Collectors.toList());
+    }
+
+    private String[] mappingFilesIn(String path) {
+        String[] files = new File(path).list((current, name) -> new File(current, name).getName().contains(".hbm.xml"));
+
+        if (null == files) {
+            return new String[0];
+        }
+
+        return files;
+    }
+
+    private Properties hibernateProperties() {
+        Properties hibernateProperties = new Properties();
+        hibernateProperties.put(AvailableSettings.HBM2DDL_AUTO, "none");
+        hibernateProperties.put(AvailableSettings.SHOW_SQL, "false");
+        hibernateProperties.put(AvailableSettings.DIALECT, "org.hibernate.dialect.MySQL8Dialect");
+
+        return hibernateProperties;
+    }
+}
