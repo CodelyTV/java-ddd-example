f90f826a7776f1b8d9d5c47168eb9ef0889d0283
Rafa Gómez <rgomezcasas@gmail.com>
2019-11-15 00:26:44 +0100

    Create all databases tables on application starts

--- src/mooc/main/resources/database/mooc.sql
+++ src/mooc/main/resources/database/mooc.sql
@@ -1,31 +1,31 @@
-CREATE TABLE `courses` (
+CREATE TABLE IF NOT EXISTS `courses` (
   `id` CHAR(36) NOT NULL,
   `name` VARCHAR(255) NOT NULL,
   `duration` VARCHAR(255) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
-CREATE TABLE `courses_counter` (
+CREATE TABLE IF NOT EXISTS `courses_counter` (
     `id` CHAR(36) NOT NULL,
     `total` INT NOT NULL,
     `existing_courses` JSON NOT NULL,
     PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
-CREATE TABLE `steps` (
+CREATE TABLE IF NOT EXISTS `steps` (
     `id` CHAR(36) NOT NULL,
     `title` VARCHAR(155) NOT NULL,
     PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
-CREATE TABLE `steps_challenges` (
+CREATE TABLE IF NOT EXISTS `steps_challenges` (
     `id` CHAR(36) NOT NULL,
     `statement` TEXT NOT NULL,
     PRIMARY KEY (`id`),
     CONSTRAINT `fk_steps_challenges__step_id` FOREIGN KEY (`id`) REFERENCES `steps` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
-CREATE TABLE `steps_videos` (
+CREATE TABLE IF NOT EXISTS `steps_videos` (
     `id` CHAR(36) NOT NULL,
     `url` VARCHAR(255) NOT NULL,
     `text` TEXT NOT NULL,
@@ -33,7 +33,7 @@ CREATE TABLE `steps_videos` (
     CONSTRAINT `fk_steps_video__step_id` FOREIGN KEY (`id`) REFERENCES `steps` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
-CREATE TABLE `domain_events` (
+CREATE TABLE IF NOT EXISTS `domain_events` (
     `id` CHAR(36) NOT NULL,
     `aggregate_id` CHAR(36) NOT NULL,
     `name` VARCHAR(255) NOT NULL,
