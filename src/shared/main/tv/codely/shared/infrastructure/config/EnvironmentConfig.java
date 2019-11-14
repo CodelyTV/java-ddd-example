12aa6d531d7805877bd2f1a08be1f36e7083fd7c
Rafa Gómez <rgomezcasas@gmail.com>
2019-11-15 00:54:48 +0100

    Allow having local environment files

--- src/shared/main/tv/codely/shared/infrastructure/config/EnvironmentConfig.java
+++ src/shared/main/tv/codely/shared/infrastructure/config/EnvironmentConfig.java
@@ -3,17 +3,25 @@ package tv.codely.shared.infrastructure.config;
 import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
 
 @Configuration
 public class EnvironmentConfig {
+    ResourceLoader resourceLoader;
+
+    public EnvironmentConfig(ResourceLoader resourceLoader) {
+        this.resourceLoader = resourceLoader;
+    }
+
     @Bean
     public Dotenv dotenv() {
-        Dotenv dotenv = Dotenv
+        Resource resource = resourceLoader.getResource("classpath:/.env.local");
+
+        return Dotenv
             .configure()
             .directory("/")
-            .filename(".env")
+            .filename(resource.exists() ? ".env.local" : ".env")
             .load();
-
-        return dotenv;
     }
 }
