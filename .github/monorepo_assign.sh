258610ea31ccd33aaa8cb6fabeea87569a56c723
Javier Ferrer González <javier.mailserio@gmail.com>
2020-03-10 12:21:42 +0100

    Parse YAML teams and directories mapping detecting if the directory has been modified

--- .github/monorepo_assign.sh
+++ .github/monorepo_assign.sh
@@ -1,15 +1,40 @@
-# git diff --name-status origin/master | grep "src/" | awk -F '/' '{print $2}' | uniq
+function directory_has_been_modified {
+  local directory=$1
+  local changed_directories=$(git diff --name-status origin/master | grep "src/" | awk -F '/' '{print $2}' | uniq)
 
-teams=$(yq r .github/monorepo_assign.yml assign | awk -F":" '{print $1}')
-for team in "${teams[@]}"; do
-  :
-  echo $team
-  yq r .github/monorepo_assign.yml assign.$team
-#  directories=$(yq r .github/monorepo_assign.yml "\'assign.$team\'")
-#  printf '%s\n' "${directories[@]}"
-#
-#  for directory in "${directories[@]}"; do
-#    :
-#    echo "$directory"
-#  done
-done
+  [[ $changed_directories == *"$directory"* ]]
+}
+
+function team_has_been_modified {
+  local IFS=$'\n'
+  local directories=($1)
+  local i
+  for (( i=0; i<${#directories[@]}; i++ )) ; do
+    local directory_without_yaml=$(echo "${directories[$i]//'- '}")
+
+    if directory_has_been_modified "$directory_without_yaml" ; then
+        echo "👍 $directory_without_yaml directory HAS been modified"
+    else
+        echo "👎 $directory_without_yaml directory has NOT been modified"
+    fi
+  done
+}
+
+function assign_to {
+  local IFS=$'\n'
+  local teams=($1)
+  local i
+  for (( i=0; i<${#teams[@]}; i++ )) ; do
+    echo "$i: ${teams[$i]}"
+
+    echo "---------------------------------"
+    directories=$(yq r -X monorepo_assign.yml "assign.${teams[$i]}")
+
+    team_has_been_modified "$directories"
+
+    echo "---------------------------------"
+  done
+}
+
+teams=$(yq r monorepo_assign.yml assign | awk -F":" '{print $1}')
+assign_to "$teams"
