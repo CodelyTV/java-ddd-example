f32dd47d547c7d7fc1bbd1be7665adc567ec0e18
Francisco Ruiz <franciscoruizlezcano@gmail.com>
2021-02-16 15:15:59 -0300

    Add compatibility in HibernateCriteriaConverter.java with Value Objects

--- src/shared/main/tv/codely/shared/infrastructure/hibernate/HibernateCriteriaConverter.java
+++ src/shared/main/tv/codely/shared/infrastructure/hibernate/HibernateCriteriaConverter.java
@@ -12,7 +12,7 @@ import java.util.stream.Collectors;
 
 public final class HibernateCriteriaConverter<T> {
     private final CriteriaBuilder                                                 builder;
-    private final HashMap<FilterOperator, BiFunction<Filter, Root<T>, Predicate>> predicateTransformers = new HashMap<FilterOperator, BiFunction<Filter, Root<T>, Predicate>>() {{
+    private final HashMap<FilterOperator, BiFunction<Filter, Root<T>, Predicate>> predicateTransformers = new HashMap<>() {{
         put(FilterOperator.EQUAL, HibernateCriteriaConverter.this::equalsPredicateTransformer);
         put(FilterOperator.NOT_EQUAL, HibernateCriteriaConverter.this::notEqualsPredicateTransformer);
         put(FilterOperator.GT, HibernateCriteriaConverter.this::greaterThanPredicateTransformer);
@@ -29,7 +29,9 @@ public final class HibernateCriteriaConverter<T> {
         CriteriaQuery<T> hibernateCriteria = builder.createQuery(aggregateClass);
         Root<T>          root              = hibernateCriteria.from(aggregateClass);
 
-        hibernateCriteria.where(formatPredicates(criteria.filters().filters(), root));
+        Predicate[] restrictions = formatPredicates(criteria.filters().filters(), root);
+
+        hibernateCriteria.where(restrictions);
 
         if (criteria.order().hasOrder()) {
             Path<Object> orderBy = root.get(criteria.order().orderBy().value());
@@ -42,15 +44,13 @@ public final class HibernateCriteriaConverter<T> {
     }
 
     private Predicate[] formatPredicates(List<Filter> filters, Root<T> root) {
-        List<Predicate> predicates = filters.stream().map(filter -> formatPredicate(
-            filter,
-            root
-        )).collect(Collectors.toList());
+        List<Predicate> predicates = filters.stream()
+            .map(filter -> formatPredicate(filter, root))
+            .collect(Collectors.toList());
 
         Predicate[] predicatesArray = new Predicate[predicates.size()];
-        predicatesArray = predicates.toArray(predicatesArray);
 
-        return predicatesArray;
+        return predicates.toArray(predicatesArray);
     }
 
     private Predicate formatPredicate(Filter filter, Root<T> root) {
@@ -60,26 +60,60 @@ public final class HibernateCriteriaConverter<T> {
     }
 
     private Predicate equalsPredicateTransformer(Filter filter, Root<T> root) {
+        if (isValueObject(filter, root)) {
+            return builder.equal(root.get(filter.field().value()).get("value"), filter.value().value());
+        }
+
         return builder.equal(root.get(filter.field().value()), filter.value().value());
     }
 
     private Predicate notEqualsPredicateTransformer(Filter filter, Root<T> root) {
+        if (isValueObject(filter, root)) {
+            return builder.notEqual(root.get(filter.field().value()).get("value"), filter.value().value());
+        }
+
         return builder.notEqual(root.get(filter.field().value()), filter.value().value());
     }
 
     private Predicate greaterThanPredicateTransformer(Filter filter, Root<T> root) {
+        if (isValueObject(filter, root)) {
+            return builder.greaterThan(root.get(filter.field().value()).get("value"), filter.value().value());
+        }
+
         return builder.greaterThan(root.get(filter.field().value()), filter.value().value());
     }
 
     private Predicate lowerThanPredicateTransformer(Filter filter, Root<T> root) {
+        if (isValueObject(filter, root)) {
+            return builder.lessThan(root.get(filter.field().value()).get("value"), filter.value().value());
+        }
+
         return builder.lessThan(root.get(filter.field().value()), filter.value().value());
     }
 
     private Predicate containsPredicateTransformer(Filter filter, Root<T> root) {
-        return builder.like(root.get(filter.field().value()), String.format("%%%s%%", filter.value().value()));
+        if (isValueObject(filter, root)) {
+            return builder.like(root.get(filter.field().value()).get("value"), filter.value().value());
+        }
+
+        return builder.like(root.get(filter.field().value()), filter.value().value());
     }
 
     private Predicate notContainsPredicateTransformer(Filter filter, Root<T> root) {
-        return builder.notLike(root.get(filter.field().value()), String.format("%%%s%%", filter.value().value()));
+        if (isValueObject(filter, root)) {
+            return builder.notLike(root.get(filter.field().value()).get("value"), filter.value().value());
+        }
+
+        return builder.notLike(root.get(filter.field().value()), filter.value().value());
+    }
+
+    private boolean isValueObject(Filter filter, Root<T> root) {
+        try {
+            Class cls = root.get(filter.field().value()).getJavaType();
+            cls.getMethod("value");
+            return true;
+        } catch (NoSuchMethodException e) {
+            return false;
+        }
     }
 }
