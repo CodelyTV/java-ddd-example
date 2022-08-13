cb28c73470ad900b1a97c3f0b654eab9994c3d84
Xisco Bibiloni <neo.xisco@gmail.com>
2022-08-13 14:48:21 +0200

    feat:Implement the new use case --> CourseNameUpdater

---
+++ src/mooc/main/tv/codely/mooc/courses/application/update/CourseNameUpdater.java
@@ -0,0 +1,30 @@
+package tv.codely.mooc.courses.application.update;
+
+import tv.codely.mooc.courses.domain.Course;
+import tv.codely.mooc.courses.domain.CourseId;
+import tv.codely.mooc.courses.domain.CourseName;
+import tv.codely.mooc.courses.domain.CourseRepository;
+import tv.codely.mooc.courses.domain.service.DomainCourseFinder;
+import tv.codely.shared.domain.Service;
+
+@Service
+public class CourseNameUpdater {
+    private final CourseRepository repository;
+    private final DomainCourseFinder domainCourseFinder;
+
+    public CourseNameUpdater(CourseRepository repository, DomainCourseFinder domainCourseFinder) {
+        this.repository = repository;
+        this.domainCourseFinder = domainCourseFinder;
+    }
+
+    public void renameCourse(final CourseId courseId, final CourseName newCourseName) {
+        final Course course = domainCourseFinder.find(courseId);
+
+        this.repository.save(buildNewCourse(course, newCourseName));
+
+    }
+
+    private Course buildNewCourse(final Course course, final CourseName newCourseName) {
+        return new Course(course.id(), newCourseName, course.duration());
+    }
+}
