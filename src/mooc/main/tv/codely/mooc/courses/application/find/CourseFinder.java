55e0876311977349778c2d8007141762816ac84e
Xisco Bibiloni <neo.xisco@gmail.com>
2022-08-13 14:47:15 +0200

    feat:Refactor CourseFinder use case, extract code that the DomainCourseFinder should execute

--- src/mooc/main/tv/codely/mooc/courses/application/find/CourseFinder.java
+++ src/mooc/main/tv/codely/mooc/courses/application/find/CourseFinder.java
@@ -4,19 +4,18 @@ import tv.codely.mooc.courses.application.CourseResponse;
 import tv.codely.mooc.courses.domain.CourseId;
 import tv.codely.mooc.courses.domain.CourseNotExist;
 import tv.codely.mooc.courses.domain.CourseRepository;
+import tv.codely.mooc.courses.domain.service.DomainCourseFinder;
 import tv.codely.shared.domain.Service;
 
 @Service
 public final class CourseFinder {
-    private final CourseRepository repository;
+    private final DomainCourseFinder domainCourseFinder;
 
     public CourseFinder(CourseRepository repository) {
-        this.repository = repository;
+        this.domainCourseFinder = new DomainCourseFinder(repository);
     }
 
     public CourseResponse find(CourseId id) throws CourseNotExist {
-        return repository.search(id)
-                         .map(CourseResponse::fromAggregate)
-                         .orElseThrow(() -> new CourseNotExist(id));
+        return CourseResponse.fromAggregate(domainCourseFinder.find(id));
     }
 }
